b0abe465a268aebed7fd0b3d7d7bd5b019a7b7e0
pwxmarco <t3chzoneyt@gmail.com>
2026-03-04 18:50:47 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,42 +1,43 @@
 export default async function handler(req, res) {
 
-const url = "https://pwthor.site/study/";
+  const target = "https://pwthor.site/study/";
 
-const response = await fetch(url,{
-headers:{
-"User-Agent":"Mozilla/5.0",
-"Accept":"text/html"
-}
-});
+  const response = await fetch(target, {
+    headers: {
+      "User-Agent": "Mozilla/5.0",
+      "Accept": "text/html"
+    }
+  });
 
-let html = await response.text();
+  let html = await response.text();
 
-/* header hide */
-html = html.replace(/<header[\s\S]*?<\/header>/gi,"");
+  /* header hide */
+  html = html.replace(/<header[\s\S]*?<\/header>/gi, "");
 
-/* nav hide */
-html = html.replace(/<nav[\s\S]*?<\/nav>/gi,"");
+  /* navbar hide */
+  html = html.replace(/<nav[\s\S]*?<\/nav>/gi, "");
 
-/* overlay */
-html = html.replace("<body>",
-`<body style="margin-top:60px;">
-<div style="
-position:fixed;
-top:0;
-left:0;
-width:100%;
-height:60px;
-background:white;
-z-index:999999;
-display:flex;
-align-items:center;
-justify-content:center;
-font-weight:bold;
-font-size:18px;">
-PW BY MARCO
-</div>`);
+  /* overlay */
+  html = html.replace("<body>", `
+  <body style="margin-top:60px;">
+  <div style="
+  position:fixed;
+  top:0;
+  left:0;
+  width:100%;
+  height:60px;
+  background:white;
+  z-index:999999;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  font-weight:bold;
+  font-size:18px;">
+  PW BY MARCO
+  </div>
+  `);
 
-res.setHeader("Content-Type","text/html");
-res.send(html);
+  res.setHeader("Content-Type", "text/html");
+  res.send(html);
 
 }
